18057bfbf0dd87487d0d35d8d0bffdcbe8bc202f
BINGMAN <yuriko.bingman@tccd.edu>
2022-02-21 14:14:33 -0600

    first commit for vuepractice4

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <HelloWorld />
+</section>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
